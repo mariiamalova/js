e1e94d40af8a54917cec8dcc5a54532a8ec91d35
mariiamalova <106603539+mariiamalova@users.noreply.github.com>
2022-07-19 21:05:41 +0300

    Delete js directory

--- js/src/main/java/com/example/exception_handling/NoSuchUserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.exception_handling;
-
-public class NoSuchUserException extends RuntimeException {
-    public NoSuchUserException(String message) {
-        super(message);
-    }
-}
